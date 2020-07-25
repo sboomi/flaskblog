f4753cf4d68c53f6f4ada9e1ebf7660aace31d72
sboomi <47070729+sboomi@users.noreply.github.com>
2020-07-25 14:08:54 +0200

    [Update] User Authentication

---
+++ fbmodules/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = '8445011cdfc685cca1e7da707553cbce'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+
+db = SQLAlchemy(app)
+
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager(app)
+login_manager.login_view = 'login'
+login_manager.login_message_category = 'info'
+
+from fbmodules import routes
