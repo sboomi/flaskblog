0bf9fffa9da205135e69c66ba90b2d8c5c276078
sboomi <47070729+sboomi@users.noreply.github.com>
2020-09-26 15:26:45 +0200

    [Update] Blueprints

--- fbmodules/__init__.py
+++ fbmodules/__init__.py
@@ -3,26 +3,35 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 from flask_mail import Mail
-import os
+from fbmodules.config import Config
 
-app = Flask(__name__)
 
-app.config['SECRET_KEY'] = '8445011cdfc685cca1e7da707553cbce'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
-app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
-app.config['MAIL_PORT'] = 587
-app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = os.environ.get('GOOGLE_MAIL')
-app.config['MAIL_PASSWORD'] = os.environ.get('GOOGLE_PWD')
+db = SQLAlchemy()
 
-db = SQLAlchemy(app)
+bcrypt = Bcrypt()
 
-bcrypt = Bcrypt(app)
-
-login_manager = LoginManager(app)
-login_manager.login_view = 'login'
+login_manager = LoginManager()
+login_manager.login_view = 'users.login'
 login_manager.login_message_category = 'info'
 
-mail = Mail(app)
+mail = Mail()
 
-from fbmodules import routes
+
+def create_app(config_class=Config):
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    db.init_app(app)
+    bcrypt.init_app(app)
+    login_manager.init_app(app)
+    mail.init_app(app)
+    
+    from fbmodules.main.routes import main
+    from fbmodules.posts.routes import posts
+    from fbmodules.users.routes import users
+
+    app.register_blueprint(main)
+    app.register_blueprint(posts)
+    app.register_blueprint(users)
+
+    return app
